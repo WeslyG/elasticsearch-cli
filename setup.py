ab82a2387c0c0c9c979d6643874573b2fb082214
WeslyG <weslyg22@gmail.com>
2019-07-13 21:14:11 +0500

    fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="weslyg22@gmail.com",
     description="Elasticsearch cli",
     url="https://github.com/WeslyG/elasticsearch-cli",
-    download_url='https://github.com/WeslyG/elasticsearch-cli/dist/elasticsearch-cli-1.0.0.tar.gz',
+    download_url='https://github.com/WeslyG/elasticsearch-cli/releases/download/1.0.0/elasticsearch-cli-1.0.0.tar.gz',
     py_modules=['main'],
     install_requires=[
         'Click',
