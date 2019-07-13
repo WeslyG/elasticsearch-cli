d0bed501475f868b2ac6ff55871b74e3b59aa6e3
WeslyG <weslyg22@gmail.com>
2019-07-14 00:29:10 +0500

    up to 1.0.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='elasticsearch-cli',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author="Wesly Allan",
     author_email="weslyg22@gmail.com",
     description="Elasticsearch cli",
     url="https://github.com/WeslyG/elasticsearch-cli",
-    download_url='https://github.com/WeslyG/elasticsearch-cli/releases/download/1.0.0/elasticsearch-cli-1.0.0.tar.gz',
+    download_url='https://github.com/WeslyG/elasticsearch-cli/releases/download/1.0.1/elasticsearch-cli-1.0.1.tar.gz',
     packages=find_packages(),
     install_requires=[
         'Click',
