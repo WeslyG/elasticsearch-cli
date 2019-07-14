21fe979825d6914962d2d2ee038b2cefe90195ff
WeslyG <weslyg22@gmail.com>
2019-07-14 12:00:59 +0500

    success upload package

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='elasticsearch-cli',
-    version='1.0.1',
+    version='1.0.3',
     license='MIT',
     author="Wesly Allan",
     author_email="weslyg22@gmail.com",
     description="Elasticsearch cli",
     url="https://github.com/WeslyG/elasticsearch-cli",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
         'Elasticsearch'
@@ -21,6 +22,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        es=main:cli
+        es=source.main:cli
     ''',
 )
