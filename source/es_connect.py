9884b30fe64fd44e7116c96a2961833555bff5e1
eaxr <61225077+eaxr@users.noreply.github.com>
2021-04-01 14:56:44 +0300

    Update es_connect.py
    Добавлено применение системных переменных из docker-compose файла.

--- source/es_connect.py
+++ source/es_connect.py
@@ -1,5 +1,26 @@
 from elasticsearch import Elasticsearch
+import os
 
+host = os.environ.get('ES_HOST')
+port = os.environ.get('ES_PORT')
+
+name = os.environ.get('ES_USERNAME')
+secret = os.environ.get('ES_PASSWORD')
+
+ssl = os.environ.get('ES_SSL')
+ssl_verify = os.environ.get('ES_SSL_VERIFY')
+ssl_ca = os.environ.get('ES_SSL_CA')
+ssl_cert = os.environ.get('ES_SSL_CERT')
+ssl_key = os.environ.get('ES_SSL_KEY')
 
 def es_connect():
-    return Elasticsearch("localhost", maxsize=25, port=9200)
+    return Elasticsearch(
+            host=host,
+            maxsize=25,
+            port=port,
+            http_auth=(name, secret),
+            use_ssl=ssl,
+            verify_certs=ssl_verify,
+            ca_certs=ssl_ca,
+            client_cert=ssl_cert,
+            client_key=ssl_key)
